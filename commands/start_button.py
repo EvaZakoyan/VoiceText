6d9854b7c0c46fbfeea0997f22aac28f04952fea
EvaZakoyan <eva.zakoyan@goya.am>
2023-07-10 19:20:20 +0400

    Modified the gitignore

--- commands/start_button.py
+++ commands/start_button.py
@@ -1,16 +1,22 @@
 # start block
-from telegram.ext import Updater, CommandHandler 
-import constants
-from bot import *
+# from telegram.ext import Updater, CommandHandler 
+# import constants
+# from bot import *
 
-updater = Updater(token=constants.BOT_TOKEN, use_context=True)
-dispatcher = updater.dispatcher
+# updater = Updater(token=constants.BOT_TOKEN, use_context=True)
+# dispatcher = updater.dispatcher
 
  
+# def start_button(update, context):
+#     context.bot.send_message(chat_id=update.effective_chat.id, text="Hello! Send me your voice message ")
+
+# start_button_handler = CommandHandler('start', start_button)
+# dispatcher.add_handler(start_button_handler)
+
+
+from telegram.ext import CommandHandler
+
 def start_button(update, context):
     context.bot.send_message(chat_id=update.effective_chat.id, text="Hello! Send me your voice message ")
 
 start_button_handler = CommandHandler('start', start_button)
-dispatcher.add_handler(start_button_handler)
-
-
