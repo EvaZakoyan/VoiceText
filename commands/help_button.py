2c33e31cab9b2ad9d5b80957048bdea22dd33390
EvaZakoyan <102632890+EvaZakoyan@users.noreply.github.com>
2023-07-10 19:59:15 +0400

    Update help_button.py

--- commands/help_button.py
+++ commands/help_button.py
@@ -1,18 +1,4 @@
 # help block
-# from telegram.ext import Updater, CommandHandler 
-# import constants
-
-
-# updater = Updater(token=constants.BOT_TOKEN, use_context=True)
-# dispatcher = updater.dispatcher
-
-
- 
-# def help_button(update, context):
-#     context.bot.send_message(chat_id=update.effective_chat.id, text="This is a simple Telegram bot that converts voice messages to text using Google Speech. Can be found at @voicemessage_to_text_bot, crated by Y.Zakoyan")
-
-# help_button_handler = CommandHandler('help', help_button)
-# dispatcher.add_handler(help_button_handler)
 
 
 from telegram.ext import CommandHandler
